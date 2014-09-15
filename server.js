1e1646dc144cc743b8945f7672683dd8c62f266b
Chris Marsh <chris.marsh@epiphanysolutions.co.uk>
2014-09-15 14:19:51 +0100

    Added websockets to run presentation on localhost

---
+++ server.js
@@ -0,0 +1,29 @@
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic('./');
+
+var server = http.createServer(function (req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+})
+var io = require('socket.io')(server);
+
+server.listen(3000, function(req, res) {
+  console.log('Server started');
+});
+
+io.on('connection', function (socket) {
+
+  // Listen for events from mobile
+  socket.on('prev', function (data) {
+    io.emit('prev', data);
+    console.log('Prev button clicked', data);
+  });
+  socket.on('next', function (data) {
+    io.emit('next', data)
+    console.log('Next button clicked', data);
+  });
+});
+
